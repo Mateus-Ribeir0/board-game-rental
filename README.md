ab06f555e1ac127e984e8d87779e89bb00e54944
Mateus Ribeiro <160142915+Mateus-Ribeir0@users.noreply.github.com>
2024-03-15 12:42:58 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🎲 Aluguel de Jogos de Tabuleiro 🎮
+
+## Sobre
+Este projeto foi desenvolvido como parte do curso de lógica de programação, oferecendo uma plataforma virtual para aluguel e devolução de jogos de tabuleiro.
+
+### Funcionalidades
+- Aluguel e devolução de jogos.
+- Biblioteca virtual para gestão dos jogos disponíveis.
+- Interface intuitiva para facilitar a navegação.
+
+## Tecnologias
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Criar função que mude o html quando clicar no botão, se tiver alugado dar opção de devolver, se não só alugar, deixando preto e branco
+// Criar uma função que funcione para todos os jogos, não apenas um.
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    // Criar variaveis que facilitem alterar a class no futuro
+    // Pegar primeiro elemento da class com o querySelector
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeGame = gameClicado.querySelector('.dashboard__item__name');
+    // Criar condicional que veja se o jogo esta alugado ou não observando a class dele.
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        alert(`Obrigado por devolver o jogo ${nomeGame.textContent}!`)
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+    
+    console.log(contarElementosComClasse('dashboard__item__img--rented'));
+}
+
+// Criar função para contar quantos jogos estão alugados.
+
+function contarElementosComClasse(classe) {
+    let quantidadeAlugado = document.querySelectorAll('ul.dashboard__items .' + classe);
+    return quantidadeAlugado.length;
+}
